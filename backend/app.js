28b8ebf45895cedbc83d34ed34cea52a161b641c
vanh <aw5318999@gmaill.com>
2026-02-26 03:27:45 +0700

    Add debug route for path investigation

--- backend/app.js
+++ backend/app.js
@@ -33,4 +33,14 @@ app.get('/nathanadmin', (req, res) => {
     res.sendFile(path.join(frontendDir, 'nathanadmin.html'));
 });
 
+app.get('/debug-paths', (req, res) => {
+    const fs = require('fs');
+    try {
+        const files = fs.readdirSync(frontendDir);
+        res.json({ frontendDir, files });
+    } catch (e) {
+        res.json({ error: e.message, frontendDir });
+    }
+});
+
 module.exports = app;
